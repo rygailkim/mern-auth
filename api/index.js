ff623577c7e972825ad8fdac700806a86872e6b9
unknown <rygailkim@gmail.com>
2024-05-08 14:15:40 +0800

    created test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 
 mongoose
@@ -15,5 +16,11 @@ mongoose
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server listening on port3000");
+  console.log("Server listening on port 3000");
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
+});
+
+app.use("/api/user", userRoutes)
